3b02b832f2649d97b399c4100fd83359ee0a1f92
LizRuz <105356375+LizRuz@users.noreply.github.com>
2022-11-15 20:01:13 +1100

    Add files via upload

---
+++ src/App/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Contact } from "../pages/Contact";
+import { Login } from "../pages/Login";
+import { PageNotFound } from "../pages/PageNotFound";
+import { Footer } from "../components/Footer"
+import { Menu } from "../components/Menu";
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Menu/>
+          <Routes>
+              <Route path="/login/" exact element={< Login />}/> 
+              <Route path="/contact/" exact element={< Contact />}/>
+              <Route path="/explore/" exact element={< Contact />}/>
+              <Route path="/*" element={< PageNotFound />}/> 
+          </Routes>
+        <Footer /> 
+      </BrowserRouter>
+  );
+}
+
+export default App;
